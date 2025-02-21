1fef38e3b563beefb58d795e233999f64895e9a0
ripon4521 <riponakondo4521@gmail.com>
2025-02-21 23:56:45 +0600

    created React Router and Setup Router. Install Inter Font

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// const queryClient = new QueryClient();
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import { router } from "./Router/Router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// import { QueryClient, QueryClientProvider } from 'react-query';
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <QueryClientProvider client={queryClient}> */}
+
+    <RouterProvider router={router} />
+
+    {/* </QueryClientProvider> */}
+  </React.StrictMode>
+);
