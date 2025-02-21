3b629be5a171fa1df42530b517e6d9fb453e86a5
ripon4521 <riponakondo4521@gmail.com>
2025-02-22 02:10:48 +0600

    created ho it works section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import HowItWorks from "../HowItWorks/HowItWorks";
 
 
 const Home = () => {
     return (
         <div>
            <Banner/>
+           <HowItWorks/>
         </div>
     );
 };
