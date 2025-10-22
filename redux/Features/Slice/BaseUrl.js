ff3c1ca46a80de34ca14c669803518347e537271
ripon4521 <riponakondo4521@gmail.com>
2025-10-22 18:52:09 +0600

    updated

--- redux/Features/Slice/BaseUrl.js
+++ redux/Features/Slice/BaseUrl.js
@@ -3,8 +3,8 @@ import { jwtDecode } from "jwt-decode";
 import { userLoggedIn, userLoggedOut } from "../Api/Auth/AuthSlice";
 
 const baseQuery = fetchBaseQuery({
-  //baseUrl: "https://sandbox.iconadmissionaid.com/api/v1/",
- baseUrl: "http://localhost:8000/api/v1/",
+  baseUrl: "https://sandbox.iconadmissionaid.com/api/v1/",
+//  baseUrl: "http://localhost:8000/api/v1/",
   credentials: "include",
   prepareHeaders: (headers) => {
     const token = localStorage.getItem("accessToken");
